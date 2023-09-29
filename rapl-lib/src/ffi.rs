61a7f034f341f6c7491f0f50f1589ee30c47ac58
Jakobzs <31919330+Jakobzs@users.noreply.github.com>
2023-09-29 10:02:14 +0200

    rapl impl mod

--- rapl-lib/src/ffi.rs
+++ rapl-lib/src/ffi.rs
@@ -1,19 +1,19 @@
 #[cfg(target_os = "linux")]
-use crate::rapl::linux::start_rapl_impl;
-#[cfg(target_os = "linux")]
-use crate::rapl::linux::stop_rapl_impl;
+mod rapl_impl {
+    pub use crate::rapl::linux::{start_rapl_impl, stop_rapl_impl};
+}
 
 #[cfg(target_os = "windows")]
-use crate::rapl::windows::start_rapl_impl;
-#[cfg(target_os = "windows")]
-use crate::rapl::windows::stop_rapl_impl;
+mod rapl_impl {
+    pub use crate::rapl::windows::{start_rapl_impl, stop_rapl_impl};
+}
 
 #[no_mangle]
 pub extern "C" fn start_rapl() {
-    start_rapl_impl();
+    rapl_impl::start_rapl_impl();
 }
 
 #[no_mangle]
 pub extern "C" fn stop_rapl() {
-    stop_rapl_impl();
+    rapl_impl::stop_rapl_impl();
 }
