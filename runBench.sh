2a205e5c4b41c264857a77dd745b6715b13ae5db
Villiam <Vjacob19@student.aau.dk>
2023-10-04 11:02:18 +0200

    renaming test.csv + running "exe" on linux

--- runBench.sh
+++ runBench.sh
@@ -1,16 +1,25 @@
 # TODO stop servies running on the computer
+echo "starting"
 
 # -- fib --
 #   Node
-node .\\benchmarks\\FibSequence\\bench.js
+echo "starting fib"
+
+node ./benchmarks/FibSequence/bench.js
+mv test.csv fib_node.csv
 
 #   Pypy
-Pypy .\\benchmarks\\FibSequence\\bench.py
+pypy ./benchmarks/FibSequence/bench.py
+mv test.csv fib_pypy.csv
 
 #   C#
 # building
-cd benchmarks\\FibSequence\\benchC#
+cd benchmarks/FibSequence/benchC#
 dotnet build # todo --release
-cd ..\\..\\..
+cd ../../..
+
 # running
-bash .\\benchmarks\\FibSequence\\benchC#\\bin\\Debug\\net7.0\\Fib.exe
+./benchmarks/FibSequence/benchC#/bin/Debug/net7.0/Fib
+mv test.csv fib_csharp.csv
+
+# TODO start services again
