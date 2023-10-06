377a1fbdb19509016ad8e9a95f993e6f1847aa7a
Jakob <31919330+Jakobzs@users.noreply.github.com>
2023-10-06 13:21:57 +0200

    removed old code

--- rapl-lib/src/ffi.rs
+++ rapl-lib/src/ffi.rs
@@ -1,17 +1,5 @@
 use crate::rapl;
 
-/*
-#[cfg(target_os = "linux")]
-mod rapl_impl {
-    pub use crate::rapl::linux::{start_rapl_impl, stop_rapl_impl};
-}
-
-#[cfg(target_os = "windows")]
-mod rapl_impl {
-    pub use crate::rapl::windowss::stop_rapl_impl;
-}
-*/
-
 #[no_mangle]
 pub extern "C" fn start_rapl() {
     rapl::start_rapl();
