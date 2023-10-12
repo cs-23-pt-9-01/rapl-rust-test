d095528ac94377f51c8303a0e668fe07eca3d232
Villiam <Vjacob19@student.aau.dk>
2023-10-12 14:14:44 +0200

    fixed loop

--- benchmarks/FibSequence/fibjava/Bench.java
+++ benchmarks/FibSequence/fibjava/Bench.java
@@ -48,10 +48,9 @@ class Bench {
         }
         */
 
-        // Loop 10 times.
         // Note that this could potentially be optimized away
         // by the compiler due to the fact that the result is not used.
-        for (int i = loop_count; i < 10; i++) {
+        for (int i = 0; i < loop_count; i++) {
             try {
                 start_rapl.invoke();
             } catch (Throwable e) {
