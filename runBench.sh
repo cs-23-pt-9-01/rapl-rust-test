9e5e4ca71786b5a1bb1de729799e66c4d05bfce2
Villiam <Vjacob19@student.aau.dk>
2023-10-16 10:16:54 +0200

    Added some description and error handling in regard to arguments (+ tabs)

--- runBench.sh
+++ runBench.sh
@@ -2,32 +2,42 @@
 
 # first argument is for stopping the logging from raspberry
 # secound argument is for stopping the kill and burn script
-
+# both arguments are strings and are set to "true" by default,
 arg1=${1:-'true'}
 arg2=${2:-'true'}
+# set them to false stop the given feature.
+
+echo $arg1
+echo $arg2
+
+# checking for invalid input
+if ([ $arg1 != 'true' ] && [ $arg1 != 'false' ]) || ([ $arg2 != 'true' ] && [ $arg2 != 'false' ])
+then
+	echo "invalid input given, 'true' or 'false'"
+	exit
+fi
 
 ##########################
 ##### Pre Benchmarks #####
 ##########################
 
-#Send start signal to Raspberry PI - await confirmation from raspberry?
-echo "starting logger"
-
 if [ $arg1 != 'false' ]
 then
-bash utils/raspberry_logger.sh 1
-sleep 10s
+	#Send start signal to Raspberry PI - await confirmation from raspberry?
+	echo "starting logger"
+	bash utils/raspberry_logger.sh 1
+	sleep 10s
 fi
 
 if [ $arg2 != 'false' ]
 then
-# Stop services and background processes
-bash utils/kill_and_burn.sh 0
+	# Stop services and background processes
+	bash utils/kill_and_burn.sh 0
 fi
 
 ##########################
 ##### Run Benchmarks #####
-########################## 
+##########################
 echo "Starting benchmarks"
 
 # Create dir for results
@@ -35,7 +45,7 @@ mkdir results
 
 # Running all benchmarks
 for f in benchRunners/*.sh; do
-  bash "$f"
+	bash "$f"
 done
 
 ###########################
@@ -44,26 +54,26 @@ done
 
 if [ $arg2 != 'false' ]
 then
-# starting services
-bash utils/kill_and_burn.sh 1
+	# starting services
+	bash utils/kill_and_burn.sh 1
 
 
-# waiting for services to start
-echo "waiting 10 secounds for services to start"
-sleep 10s
+	# waiting for services to start
+	echo "waiting 10 secounds for services to start"
+	sleep 10s
 fi
 
 if [ $arg1 != 'false' ]
 then
-# Send stop signal to Raspberry PI
-bash utils/raspberry_logger.sh 0
+	# Send stop signal to Raspberry PI
+	bash utils/raspberry_logger.sh 0
 fi
 
 
 if [ $arg2 != 'false' ]
 then
-# Send results data to Raspberry PI
-bash utils/send_results.sh
+	# Send results data to Raspberry PI
+	bash utils/send_results.sh
 fi
 
 
