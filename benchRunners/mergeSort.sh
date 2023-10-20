49b1dea1fc478ae80e9a7f3ccc404e392689b5df
Villiamfj <villiamfj@hotmail.dk>
2023-10-20 11:40:26 +0200

    File path was wrong in the run script

--- benchRunners/mergeSort.sh
+++ benchRunners/mergeSort.sh
@@ -21,11 +21,11 @@ sleep 5s
 bash utils/append_to_latest_csv.sh "Pypy$testName"
 
 #   C#
-dotnet run --project ./benchmarks/$folder/benchC#/Fib.csproj --configuration Release $mergeInput $count
+dotnet run --project ./benchmarks/$folder/benchC#/MergeSort.csproj --configuration Release $mergeInput $count
 sleep 5s
 bash utils/append_to_latest_csv.sh "Csharp$testName"
 
 #   Java
-java --enable-native-access=ALL-UNNAMED --enable-preview --source 21 ./benchmarks/$folder/fibjava/Bench.java $mergeInput $count
+java --enable-native-access=ALL-UNNAMED --enable-preview --source 21 ./benchmarks/$folder/java/Bench.java $mergeInput $count
 sleep 5s
 bash utils/append_to_latest_csv.sh "Java$testName"
