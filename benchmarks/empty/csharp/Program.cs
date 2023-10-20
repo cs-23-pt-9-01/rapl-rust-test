53672f09940a8c5dc49339ba6fbdfe1469496abd
Jakobzs <31919330+Jakobzs@users.noreply.github.com>
2023-10-20 10:29:30 +0200

    rename csharp to empty

--- benchmarks/empty/csharp/Program.cs
+++ benchmarks/empty/csharp/Program.cs
@@ -11,39 +11,17 @@ using System.Runtime.InteropServices;
 #endif
 
 string[] arguments = Environment.GetCommandLineArgs();
-uint count = uint.Parse(arguments[2]);
-uint fibVal = uint.Parse(arguments[1]);
+uint count = uint.Parse(arguments[1]);
 
-// DLL imports
 [DllImport(pathToLib)]
 static extern int start_rapl();
 
 [DllImport(pathToLib)]
 static extern void stop_rapl();
 
-// test method
-static ulong Fib(uint x)
-{
-    if (x == 0) return 0;
-
-    ulong prev = 0;
-    ulong next = 1;
-    for (int i = 1; i < x; i++)
-    {
-        ulong sum = prev + next;
-        prev = next;
-        next = sum;
-    }
-    return next;
-}
-
-//testing
 for (int i = 0; i < count; i++)
 {
     start_rapl();
-
-    Fib(fibVal);
-
     stop_rapl();
 }
 Console.WriteLine("job done");
