e08fb1e38cd26145b3481697bb46d4153e30ac45
Jakobzs <31919330+Jakobzs@users.noreply.github.com>
2023-10-20 12:03:31 +0200

    Compile with -O3

--- benchmarks/empty/c/bench.c
+++ benchmarks/empty/c/bench.c
@@ -1,4 +1,4 @@
-// gcc benchmarks/empty/c/bench.c -o benchmarks/empty/c/bench -L./target/release -lrapl_lib -Wl,-rpath=./target/release && ./benchmarks/empty/c/bench
+// gcc benchmarks/empty/c/bench.c -O3 -o benchmarks/empty/c/bench -L./target/release -lrapl_lib -Wl,-rpath=./target/release && ./benchmarks/empty/c/bench
 
 void start_rapl();
 void stop_rapl();
