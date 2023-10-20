61761893f69b08ab90f628fd76373acce20a5f16
Jakobzs <31919330+Jakobzs@users.noreply.github.com>
2023-10-20 10:25:18 +0200

    lots of rename

---
+++ benchmarks/empty/csharp/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+// inspired from https://stackoverflow.com/questions/24374658/check-the-operating-system-at-compile-time 
+#if _LINUX
+    const string pathToLib = @"target/release/librapl_lib.so";
+#elif _WINDOWS
+    const string pathToLib = @"target\release\rapl_lib.dll";
+#else
+    const string pathToLib = "none";
+#endif
+
+string[] arguments = Environment.GetCommandLineArgs();
+uint count = uint.Parse(arguments[2]);
+uint fibVal = uint.Parse(arguments[1]);
+
+// DLL imports
+[DllImport(pathToLib)]
+static extern int start_rapl();
+
+[DllImport(pathToLib)]
+static extern void stop_rapl();
+
+// test method
+static ulong Fib(uint x)
+{
+    if (x == 0) return 0;
+
+    ulong prev = 0;
+    ulong next = 1;
+    for (int i = 1; i < x; i++)
+    {
+        ulong sum = prev + next;
+        prev = next;
+        next = sum;
+    }
+    return next;
+}
+
+//testing
+for (int i = 0; i < count; i++)
+{
+    start_rapl();
+
+    Fib(fibVal);
+
+    stop_rapl();
+}
+Console.WriteLine("job done");
--- benchmarks/empty/python/bench.py
+++ benchmarks/empty/python/bench.py
@@ -6,7 +6,6 @@ test_count = int(sys.argv[1])
 lib_path = "target\\release\\rapl_lib.dll" if platform.system(
 ) == "Windows" else "target/release/librapl_lib.so"
 
-# start lib
 dll = cdll.LoadLibrary(lib_path)
 
 for i in range(test_count):
