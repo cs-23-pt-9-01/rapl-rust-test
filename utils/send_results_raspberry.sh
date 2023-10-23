94e65b0d8d2b2fe818de10f41e89d9107025ae44
Simon <sabj17@student.aau.dk>
2023-10-23 12:08:11 +0200

    delete code in comments

--- utils/send_results_raspberry.sh
+++ utils/send_results_raspberry.sh
@@ -6,4 +6,3 @@ IP="192.168.0.5"
 # Send entire results dir to Raspberry Pi
 # Uses -i ~/.ssh/id_rsa for public key use, rather than password auth
 scp results/* $HOSTNAME@$IP:results/ 
-#scp -r /home/seff/rapl-interface/results $HOSTNAME@$IP:/home/$HOSTNAME/
