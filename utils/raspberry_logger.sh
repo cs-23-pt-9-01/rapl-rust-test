931cdbf0016a244496d14333627d3f92e6517f67
Simon <sabj17@student.aau.dk>
2023-10-23 10:09:26 +0200

    Fix endif raspberry_logger

--- utils/raspberry_logger.sh
+++ utils/raspberry_logger.sh
@@ -9,7 +9,7 @@ if [ "$1" == 1 ]; then
   COMMAND="python /$HOME/BenchmanagemenRaspbPi/kasa_energy_consumption.py"
 elif [ "$1" == 0 ]; then
   COMMAND="python /$HOME/BenchmanagemenRaspbPi/temp_socket_testing_manager.py"
-
+fi
 # Call Raspberry PI with command
 # Uses -i ~/.ssh/id_rsa for public key use, rather than password auth
 ssh -i /home/seff/.ssh/id_rsa $HOSTNAME@$IP $COMMAND
