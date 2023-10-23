9b7da40c69c7c5afa6e439500a4f809eb018d03b
Jakobzs <31919330+Jakobzs@users.noreply.github.com>
2023-10-23 09:16:43 +0200

    put C test at the top

--- benchRunners/empty.sh
+++ benchRunners/empty.sh
@@ -2,6 +2,11 @@ count=1000
 
 echo "starting empty"
 
+#   C
+gcc benchmarks/empty/c/bench.c -O3 -o benchmarks/empty/c/bench -L./target/release -lrapl_lib -Wl,-rpath=./target/release && ./benchmarks/empty/c/bench $count
+sleep 5s
+bash utils/append_to_latest_csv.sh "Cempty"
+
 #   Node
 node ./benchmarks/empty/javascript/bench.js $count
 sleep 5s
@@ -26,8 +31,3 @@ bash utils/append_to_latest_csv.sh "CsharpEmpty"
 java --enable-native-access=ALL-UNNAMED --enable-preview --source 21 ./benchmarks/empty/java/Bench.java $count
 sleep 5s
 bash utils/append_to_latest_csv.sh "JavaEmpty"
-
-#   C
-gcc benchmarks/empty/c/bench.c -O3 -o benchmarks/empty/c/bench -L./target/release -lrapl_lib -Wl,-rpath=./target/release && ./benchmarks/empty/c/bench $count
-sleep 5s
-bash utils/append_to_latest_csv.sh "Cempty"
