b5fa07bda955e0d8f91e3f8389b288b917bfb460
Simon <sabj17@student.aau.dk>
2023-10-23 10:16:11 +0200

    take 3

--- utils/raspberry_logger.sh
+++ utils/raspberry_logger.sh
@@ -6,9 +6,9 @@ IP="192.168.0.5"
 
 # Commands for the raspberry pi
 if [ "$1" == 1 ]; then
-  COMMAND="python $HOME/BenchmanagemenRaspbPi/kasa_energy_consumption.py"
+  COMMAND="python BenchManagementRaspbPI/kasa_energy_consumption.py"
 elif [ "$1" == 0 ]; then
-  COMMAND="python $HOME/BenchmanagemenRaspbPi/temp_socket_testing_manager.py"
+  COMMAND="python BenchManagementRaspbPI/temp_socket_testing_manager.py"
 fi
 # Call Raspberry PI with command
 # Uses -i ~/.ssh/id_rsa for public key use, rather than password auth
