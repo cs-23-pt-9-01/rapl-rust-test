c589c83cf1c812f2ff62125d5276eb28e6050c04
Mads <57893682+Mads518d@users.noreply.github.com>
2023-10-24 09:54:50 +0200

    Changed order to make testing faster

--- benchRunners/nbody.sh
+++ benchRunners/nbody.sh
@@ -3,37 +3,45 @@ count=1 #Testing only #TODO: change to actually useful number
 
 echo "starting N-Body"
 
-#   Node
-node ./benchmarks/N-Body/bench.js $Body_Count $count
-sleep 5s
-bash utils/append_to_latest_csv.sh "NodeNBody"
-
-#   Python
-python3 ./benchmarks/N-Body/bench.py $Body_Count $count
-sleep 5s
-bash utils/append_to_latest_csv.sh "PythonNBody"
-
-#   Pypy
-pypy ./benchmarks/N-Body/bench.py $Body_Count $count
-sleep 5s
-bash utils/append_to_latest_csv.sh "PypyNBody"
-
-#   C#
-dotnet run --project ./benchmarks/N-Body/benchC#/N-Body/N-Body.csproj --configuration Release $Body_Count $count
-sleep 5s
-bash utils/append_to_latest_csv.sh "CsharpNBody"
-
-#   Java
-java --enable-native-access=ALL-UNNAMED --enable-preview --source 21 ./benchmarks/N-Body/benchJava/N-Body/src/Bench.java $Body_Count $count
-sleep 5s
-bash utils/append_to_latest_csv.sh "JavaNBody"
-
 #   C
+echo - Starting C
 gcc -fomit-frame-pointer benchmarks/N-Body/benchC/Also_better_than_rust.c -O3 -o benchmarks/N-Body/benchC/Also_better_than_rust -L./target/release -lrapl_lib -Wl,-rpath=./target/release && ./benchmarks/N-Body/benchC/Also_better_than_rust.c $Body_Count $count
 sleep 5s
 bash utils/append_to_latest_csv.sh "CNBody"
 
 #   C++
+echo - Starting C++
 g++ -fomit-frame-pointer -std=c++17 benchmarks/N-Body/benchC++/better_than_rust.cpp -O3 -o benchmarks/N-Body/benchC++/better_than_rust -L./target/release -lrapl_lib -Wl,-rpath=./target/release && ./benchmarks/N-Body/benchC++/better_than_rust.cpp $Body_Count $count
 sleep 5s
 bash utils/append_to_latest_csv.sh "CppNBody"
+
+#   Node
+echo - Starting Node.js
+node ./benchmarks/N-Body/bench.js $Body_Count $count
+sleep 5s
+bash utils/append_to_latest_csv.sh "NodeNBody"
+
+#   Python
+echo - Starting Python
+python3 ./benchmarks/N-Body/bench.py $Body_Count $count
+sleep 5s
+bash utils/append_to_latest_csv.sh "PythonNBody"
+
+#   Pypy
+echo - Starting PyPy
+pypy ./benchmarks/N-Body/bench.py $Body_Count $count
+sleep 5s
+bash utils/append_to_latest_csv.sh "PypyNBody"
+
+#   C#
+echo - Starting C#
+dotnet run --project ./benchmarks/N-Body/benchC#/N-Body/N-Body.csproj --configuration Release $Body_Count $count
+sleep 5s
+bash utils/append_to_latest_csv.sh "CsharpNBody"
+
+#   Java
+echo - Starting Java
+java --enable-native-access=ALL-UNNAMED --enable-preview --source 21 ./benchmarks/N-Body/benchJava/N-Body/src/Bench.java $Body_Count $count
+sleep 5s
+bash utils/append_to_latest_csv.sh "JavaNBody"
+
