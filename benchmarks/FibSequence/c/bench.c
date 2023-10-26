8ebc715cf365965b2aa35ca7bfdaa2439103e6af
Villiam <Vjacob19@student.aau.dk>
2023-10-26 13:44:30 +0200

    recursive fib c

--- benchmarks/FibSequence/c/bench.c
+++ benchmarks/FibSequence/c/bench.c
@@ -5,14 +5,8 @@ void start_rapl();
 void stop_rapl();
 
 // test method
-long long int fibb(int n) {
-	int fnow = 0, fnext = 1, tempf;
-	while(--n>0){
-		tempf = fnow + fnext;
-		fnow = fnext;
-		fnext = tempf;
-	}
-	return fnext;	
+long long fibb(long long a, long long b, int n) {
+    return (--n>0)?(fibb(b, a+b, n)):(a);
 }
 
 int main(int argc, char *argv[]) {
@@ -21,12 +15,12 @@ int main(int argc, char *argv[]) {
 
     for (int i = 0; i < count; i++) {
         start_rapl();
-        long long int result = fibb(fibParam);
+        long long int result = fibb(0,1,fibParam);
         stop_rapl();
 
         // stopping compiler optimization
         if (result < 42){
-            printf("Result: %lld\n", result);
+            printf("%lld\n", result);
         }
     }
     return 0;
