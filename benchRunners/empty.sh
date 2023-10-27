85416fc6a2c1837f3ffa6a631bc66965569d1d89
Jakob <31919330+Jakobzs@users.noreply.github.com>
2023-10-27 11:11:27 +0200

    correct test for empty

--- benchRunners/empty.sh
+++ benchRunners/empty.sh
@@ -7,7 +7,7 @@ echo
 
 #   C
 echo --- Starting C ---
-gcc benchmarks/empty/c/bench.c -O3 -o benchmarks/empty/c/bench -L./target/release -lrapl_lib -Wl,-rpath=./target/release && ./benchmarks/empty/c/bench $count
+gcc benchmarks/$folder/c/bench.c -O3 -o benchmarks/$folder/c/bench -L./target/release -lrapl_lib -Wl,-rpath=./target/release && ./benchmarks/$folder/c/bench $count
 sleep 5s
 bash utils/append_to_latest_csv.sh "CEmpty"
 echo --- C Done ---
@@ -15,49 +15,49 @@ echo
 
 #   C++
 echo --- Starting C++ ---
-g++ benchmarks/empty/cpp/bench.cpp -O3 -o benchmarks/empty/cpp/bench -L./target/release -lrapl_lib -Wl,-rpath=./target/release && ./benchmarks/empty/cpp/bench $count
+g++ benchmarks/$folder/cpp/bench.cpp -O3 -o benchmarks/empty/cpp/bench -L./target/release -lrapl_lib -Wl,-rpath=./target/release && ./benchmarks/$folder/cpp/bench $count
 sleep 5s
-bash utils/append_to_latest_csv.sh "CppEmpty"
+bash utils/append_to_latest_csv.sh "Cpp$testName"
 echo --- C++ Done ---
 echo
 
 #   Node
 echo --- Starting JavaScript ---
-node ./benchmarks/empty/javascript/bench.js $count
+node ./benchmarks/$folder/javascript/bench.js $count
 sleep 5s
-bash utils/append_to_latest_csv.sh "NodeEmpty"
+bash utils/append_to_latest_csv.sh "Node$testName"
 echo --- JavaScript Done ---
 echo
 
 #   Python
 echo --- Starting Python ---
-python3 ./benchmarks/empty/python/bench.py $count
+python3 ./benchmarks/$folder/python/bench.py $count
 sleep 5s
-bash utils/append_to_latest_csv.sh "PythonEmpty"
+bash utils/append_to_latest_csv.sh "Python$testName"
 echo --- Python Done ---
 echo
 
 #   Pypy
 echo --- Starting PyPy ---
-pypy ./benchmarks/empty/python/bench.py $count
+pypy ./benchmarks/$folder/python/bench.py $count
 sleep 5s
-bash utils/append_to_latest_csv.sh "PypyEmpty"
+bash utils/append_to_latest_csv.sh "Pypy$testName"
 echo --- PyPy Done ---
 echo
 
 #   C#
 echo --- Starting C# ---
-dotnet run --project ./benchmarks/empty/csharp/Empty.csproj --configuration Release $count
+dotnet run --project ./benchmarks/$folder/csharp/Empty.csproj --configuration Release $count
 sleep 5s
-bash utils/append_to_latest_csv.sh "CsharpEmpty"
+bash utils/append_to_latest_csv.sh "Csharp$testName"
 echo --- C# Done ---
 echo
 
 #   Java
 echo --- Starting Java ---
-java --enable-native-access=ALL-UNNAMED --enable-preview --source 21 ./benchmarks/empty/java/Bench.java $count
+java --enable-native-access=ALL-UNNAMED --enable-preview --source 21 ./benchmarks/$folder/java/Bench.java $count
 sleep 5s
-bash utils/append_to_latest_csv.sh "JavaEmpty"
+bash utils/append_to_latest_csv.sh "Java$testName"
 echo --- Java Done ---
 echo
 
