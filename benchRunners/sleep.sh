7d0ade45f7fdd5c1ce54d0e0cd60888690ddd06e
Jakob <31919330+Jakobzs@users.noreply.github.com>
2023-10-27 10:46:08 +0200

    use proper test path....

--- benchRunners/sleep.sh
+++ benchRunners/sleep.sh
@@ -7,7 +7,7 @@ echo
 
 #   C
 echo --- Starting C ---
-gcc benchmarks/empty/c/bench.c -O3 -o benchmarks/empty/c/bench -L./target/release -lrapl_lib -Wl,-rpath=./target/release && ./benchmarks/empty/c/bench $count 5
+gcc benchmarks/sleep/c/bench.c -O3 -o benchmarks/sleep/c/bench -L./target/release -lrapl_lib -Wl,-rpath=./target/release && ./benchmarks/empty/c/bench $count 5
 sleep 5s
 bash utils/append_to_latest_csv.sh "CEmpty"
 echo --- C Done ---
@@ -15,7 +15,7 @@ echo
 
 #   C++
 echo --- Starting C++ ---
-g++ benchmarks/empty/cpp/bench.cpp -O3 -o benchmarks/empty/cpp/bench -L./target/release -lrapl_lib -Wl,-rpath=./target/release && ./benchmarks/empty/cpp/bench $count 5
+g++ benchmarks/sleep/cpp/bench.cpp -O3 -o benchmarks/sleep/cpp/bench -L./target/release -lrapl_lib -Wl,-rpath=./target/release && ./benchmarks/empty/cpp/bench $count 5
 sleep 5s
 bash utils/append_to_latest_csv.sh "CppEmpty"
 echo --- C++ Done ---
@@ -23,7 +23,7 @@ echo
 
 #   Node
 echo --- Starting JavaScript ---
-node ./benchmarks/empty/javascript/bench.js $count 5
+node ./benchmarks/sleep/javascript/bench.js $count 5
 sleep 5s
 bash utils/append_to_latest_csv.sh "NodeEmpty"
 echo --- JavaScript Done ---
@@ -31,7 +31,7 @@ echo
 
 #   Python
 echo --- Starting Python ---
-python3 ./benchmarks/empty/python/bench.py $count 5
+python3 ./benchmarks/sleep/python/bench.py $count 5
 sleep 5s
 bash utils/append_to_latest_csv.sh "PythonEmpty"
 echo --- Python Done ---
@@ -39,7 +39,7 @@ echo
 
 #   Pypy
 echo --- Starting PyPy ---
-pypy ./benchmarks/empty/python/bench.py $count 5
+pypy ./benchmarks/sleep/python/bench.py $count 5
 sleep 5s
 bash utils/append_to_latest_csv.sh "PypyEmpty"
 echo --- PyPy Done ---
@@ -47,7 +47,7 @@ echo
 
 #   C#
 echo --- Starting C# ---
-dotnet run --project ./benchmarks/empty/csharp/Empty.csproj --configuration Release $count 5
+dotnet run --project ./benchmarks/sleep/csharp/Sleep.csproj --configuration Release $count 5
 sleep 5s
 bash utils/append_to_latest_csv.sh "CsharpEmpty"
 echo --- C# Done ---
@@ -55,7 +55,7 @@ echo
 
 #   Java
 echo --- Starting Java ---
-java --enable-native-access=ALL-UNNAMED --enable-preview --source 21 ./benchmarks/empty/java/Bench.java $count 5
+java --enable-native-access=ALL-UNNAMED --enable-preview --source 21 ./benchmarks/sleep/java/Bench.java $count 5
 sleep 5s
 bash utils/append_to_latest_csv.sh "JavaEmpty"
 echo --- Java Done ---
