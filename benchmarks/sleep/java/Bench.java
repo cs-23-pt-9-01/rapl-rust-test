97227a1e85d4e991502578f090cafd21b50fdeb9
Jakob <31919330+Jakobzs@users.noreply.github.com>
2023-10-27 10:55:58 +0200

    sleep properly in java

--- benchmarks/sleep/java/Bench.java
+++ benchmarks/sleep/java/Bench.java
@@ -63,7 +63,7 @@ class Bench {
             }
 
             try {
-                Thread.sleep(sleep_time);
+                Thread.sleep(sleep_time*1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
