92c8583d5a0eb5628ab2413f000129957afc5474
Jakob <31919330+Jakobzs@users.noreply.github.com>
2023-10-27 10:58:19 +0200

    test javascript sleep

--- benchmarks/sleep/javascript/bench.js
+++ benchmarks/sleep/javascript/bench.js
@@ -1,6 +1,7 @@
 const os = require("os");
 
 const runCount = process.argv[2];
+const sleep_time = process.argv[3];
 const libPath = os.platform() == "win32" ?
   "target\\release\\rapl_lib.dll" :
   "target/release/librapl_lib.so"
@@ -13,5 +14,12 @@ const stop = lib.func('void stop_rapl()');
 
 for (let i = 0; i < runCount; i++) {
   start();
+
+  const start_time = Date.now();
+  let now = start_time;
+  while (now - start_time < sleep_time * 1000) {
+    now = Date.now();
+  }
+
   stop();
 }
