2653763c849f8257e05a08244a384e663586a9b2
Villiam <Vjacob19@student.aau.dk>
2023-11-03 09:31:14 +0100

    Update raspberry_logger.sh

--- utils/raspberry_logger.sh
+++ utils/raspberry_logger.sh
@@ -3,8 +3,9 @@
 HOSTNAME="seff_jr"
 IP="192.168.0.5"
 
-COMMAND="bash BenchManagementRaspbPI/logging_manager.sh"
+# prints -> log.out
+COMMAND="nohup bash BenchManagementRaspbPI/logging_manager.sh $1 > log.out"
 
 # Call Raspberry PI with command
 # Uses -i ~/.ssh/id_rsa for public key use, rather than password auth
-ssh -i $HOME/.ssh/id_rsa $HOSTNAME@$IP $COMMAND $1 &
+ssh  -i $HOME/.ssh/id_rsa $HOSTNAME@$IP $COMMAND &
