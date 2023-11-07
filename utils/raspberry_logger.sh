6ddd16a1eae95780937cc98cefb7b86352250027
Simon <sabj17@student.aau.dk>
2023-11-07 15:26:52 +0100

    Update raspberry_logger.sh

--- utils/raspberry_logger.sh
+++ utils/raspberry_logger.sh
@@ -5,7 +5,7 @@ PORT=":5000"
 
 #Send http request to raspberry to start or stop logging
 #$1: "start" or "stop"
-if [ "$1" == "start"  || "$1" == "stop" ]; then
+if [ "$1" == "start"  ]|| [ "$1" == "stop" ]; then
     curl http://$IP$PORT/$1
 else
     echo Bad input: "$1", expected "start" or "stop"
