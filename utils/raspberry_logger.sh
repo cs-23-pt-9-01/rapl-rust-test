b042cf73d2e8a602236715b42fc3aa37bbd3b1d6
Simon <sabj17@student.aau.dk>
2023-11-07 11:30:40 +0100

    Update to curl cmd instead of ssh
    Updated raspberry_logger to use curl.
    Updated runBench to send start and stop commands

--- utils/raspberry_logger.sh
+++ utils/raspberry_logger.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-HOSTNAME="seff_jr"
+#HOSTNAME="seff_jr"
 IP="192.168.0.5"
+PORT=":5000"
 
 # Command to run on Raspberry PI
 # nohup is used to stop hanging on the session and prints goes to log.out
-COMMAND="nohup bash BenchManagementRaspbPI/logging_manager.sh $1 > log.out"
+#COMMAND="nohup bash BenchManagementRaspbPI/logging_manager.sh $1 > log.out"
 
 # Call Raspberry PI with command
 # Uses -i ~/.ssh/id_rsa for public key use, rather than password auth
-ssh -i $HOME/.ssh/id_rsa $HOSTNAME@$IP $COMMAND &
+#ssh -i $HOME/.ssh/id_rsa $HOSTNAME@$IP $COMMAND &
+
+curl http://$IP$PORT/$1
