3af5d52ab0afe3627f57e3e5c293327d10472e60
Simon <sabj17@student.aau.dk>
2023-11-07 12:52:29 +0100

    Update raspberry_logger.sh
    Removed old implementation

--- utils/raspberry_logger.sh
+++ utils/raspberry_logger.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-#HOSTNAME="seff_jr"
 IP="192.168.0.5"
 PORT=":5000"
 
-# Command to run on Raspberry PI
-# nohup is used to stop hanging on the session and prints goes to log.out
-#COMMAND="nohup bash BenchManagementRaspbPI/logging_manager.sh $1 > log.out"
-
-# Call Raspberry PI with command
-# Uses -i ~/.ssh/id_rsa for public key use, rather than password auth
-#ssh -i $HOME/.ssh/id_rsa $HOSTNAME@$IP $COMMAND &
-
+#Send http request to raspberry to start or stop logging
+#$1: "start" or "stop"
 curl http://$IP$PORT/$1
