0c9b333d9346c0ea494f5f75c23514cddb8660d1
Jakob <31919330+Jakobzs@users.noreply.github.com>
2023-11-20 13:40:46 +0100

    Update base64.sh

--- benchRunners/base64.sh
+++ benchRunners/base64.sh
@@ -7,6 +7,16 @@ count=1
 echo "!!! Starting $testName !!!"
 echo
 
+#   C
+gcc benchmarks/$folder/c/bench.c -O3 -o benchmarks/$folder/c/bench -L./target/release -lrapl_lib -Wl,-rpath=./target/release
+cmd="./benchmarks/$folder/c/bench $count"
+runbenchmark "C" $testName "$cmd"
+
+#   C++
+g++ benchmarks/$folder/cpp/bench.cpp -O3 -o benchmarks/$folder/cpp/bench -L./target/release -lrapl_lib -Wl,-rpath=./target/release
+cmd="./benchmarks/$folder/cpp/bench $count"
+runbenchmark "Cpp" $testName "$cmd"
+
 #   Node
 cmd="node ./benchmarks/$folder/javascript/bench.js $count"
 runbenchmark "Node" $testName "$cmd"
@@ -26,16 +36,5 @@ runbenchmark "Csharp" $testName "$cmd"
 #   Java
 cmd="java --enable-native-access=ALL-UNNAMED --enable-preview --source 21 ./benchmarks/$folder/java/Bench.java $count"
 runbenchmark "Java" $testName "$cmd"
-
-#   C
-gcc benchmarks/$folder/c/bench.c -O3 -o benchmarks/$folder/c/bench -L./target/release -lrapl_lib -Wl,-rpath=./target/release
-cmd="./benchmarks/$folder/c/bench $count"
-runbenchmark "C" $testName "$cmd"
-
-
-#   C++
-g++ benchmarks/$folder/cpp/bench.cpp -O3 -o benchmarks/$folder/cpp/bench -L./target/release -lrapl_lib -Wl,-rpath=./target/release
-cmd="./benchmarks/$folder/cpp/bench $count"
-runbenchmark "Cpp" $testName "$cmd"
 
 echo "!!! Finished $testName !!!"
