098e712cc0526e79893bb8f254f1b79fa6a8ca02
Jakobzs <31919330+Jakobzs@users.noreply.github.com>
2023-11-22 12:15:19 +0100

    base64 java

--- benchmarks/base64/java/Bench.java
+++ benchmarks/base64/java/Bench.java
@@ -48,7 +48,39 @@ class Bench {
                 e.printStackTrace();
             }
 
-            // Do something
+            final var str = new byte[STR_SIZE];
+            Arrays.fill(str, (byte)'a');
+            final var str2 = enc.encodeToString(str);
+            final var encoded = str2.getBytes();
+            final var b_arr = dec.decode(encoded);
+
+            var s_encoded = 0;
+            final var t = System.nanoTime();
+            for (var i = 0 ; i < TRIES ; i++) {
+                s_encoded += enc.encodeToString(str).length();
+            }
+            final var t_encoded = (System.nanoTime() - t) / 1e9;
+
+            var s_decoded = 0;
+            final var t1 = System.nanoTime();
+            for (var i = 0 ; i < TRIES ; i++) {
+                s_decoded += dec.decode(encoded).length;
+            }
+            final var t_decoded = (System.nanoTime() - t1) / 1e9;
+
+            System.out.println(
+                String.format(
+                    "encode %s... to %s...: %d, %.2f",
+                    new String(Arrays.copyOf(str, 4)),
+                    str2.substring(0, 4),
+                    s_encoded, t_encoded));
+
+            System.out.println(
+                String.format("decode %s... to %s...: %d, %.2f",
+                            str2.substring(0, 4),
+                            new String(Arrays.copyOf(b_arr, 4)),
+                            s_decoded, t_decoded));
+            System.out.println("overall time: " + (t_encoded + t_decoded) + "s");
 
             try {
                 stop_rapl.invoke();
