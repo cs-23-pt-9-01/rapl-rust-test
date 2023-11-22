54a2823fa4d9aff6860136ea07cf45d6911d9329
Jakobzs <31919330+Jakobzs@users.noreply.github.com>
2023-11-22 11:11:31 +0100

    reenable rapl stuff

--- benchmarks/base64/csharp/Program.cs
+++ benchmarks/base64/csharp/Program.cs
@@ -12,8 +12,7 @@ using System.Text;
 #endif
 
 string[] arguments = Environment.GetCommandLineArgs();
-//uint count = uint.Parse(arguments[1]);
-uint count = 2;
+uint count = uint.Parse(arguments[1]);
 
 [DllImport(pathToLib)]
 static extern int start_rapl();
@@ -23,18 +22,17 @@ static extern void stop_rapl();
 
 for (int i = 0; i < count; i++)
 {
-    //start_rapl();
+    start_rapl();
 
     var STR_SIZE = 131072;
     var str1 = Encoding.UTF8.GetBytes(new String('a', STR_SIZE));
     var str2 = Convert.ToBase64String(str1);
     var str3 = Convert.FromBase64String(str2);
 
-    Console.WriteLine(str1);
+    //Console.WriteLine(str1);
     //Console.WriteLine(str2);
-    Console.WriteLine(str3);
+    //Console.WriteLine(str3);
+    //File.WriteAllText("testy.txt", str2);
 
-    File.WriteAllText("testy.txt", str2);
-
-    //stop_rapl();
+    stop_rapl();
 }
