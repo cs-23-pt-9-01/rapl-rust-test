f7c0a2ab1e4f719e5761b20d7842687a987ffaab
Jakobzs <31919330+Jakobzs@users.noreply.github.com>
2023-11-22 11:05:18 +0100

    Cleanup cpp ver

--- benchmarks/base64/cpp/bench.cpp
+++ benchmarks/base64/cpp/bench.cpp
@@ -99,56 +99,12 @@ bool operator==(const bio_string &lhs, const bio_string &rhs) {
 }
 
 int main() {
-  const char *fixtures[][2] = {{"hello", "aGVsbG8="}, {"world", "d29ybGQ="}};
-  const int num_fixtures = sizeof(fixtures) / sizeof(fixtures[0]);
-  for (auto i = 0; i < num_fixtures; ++i) {
-    const bio_string src(fixtures[i][0]);
-    const bio_string dst(fixtures[i][1]);
-
-	/*
-    const auto encoded = src.base64_encode();
-    if (encoded != dst) {
-      cerr << encoded.substr() << " != " << dst.substr() << endl;
-      exit(EXIT_FAILURE);
-    }
-
-    const auto decoded = dst.base64_decode();
-    if (decoded != src) {
-      cerr << decoded.substr() << " != " << src.substr() << endl;
-      exit(EXIT_FAILURE);
-    }
-	*/
-  }
-
   const auto STR_SIZE = 131072;
   const auto TRIES = 8192;
 
   const bio_string str("a", STR_SIZE);
   const auto str2 = str.base64_encode();
   const auto str3 = str2.base64_decode();
-
-  auto s_encoded = 0;
-  auto t_encoded = 0.0;
-  auto s_decoded = 0;
-  auto t_decoded = 0.0;
-
-  /*
-  notifying_invoke(
-      [&]() {
-        const auto t = clock();
-        for (auto i = 0; i < TRIES; i++) {
-          s_encoded += str.base64_encode().length();
-        }
-        t_encoded = (float)(clock() - t) / CLOCKS_PER_SEC;
-
-        const auto t1 = clock();
-        for (auto i = 0; i < TRIES; i++) {
-          s_decoded += str2.base64_decode().length();
-        }
-        t_decoded = (float)(clock() - t1) / CLOCKS_PER_SEC;
-      },
-      "C++/{} (libcrypto)", COMPILER);
-	*/
 
   cout << fixed;
   cout << "encode " << str.substr(0, 4) << "... to " << str2.substr(0, 4)
