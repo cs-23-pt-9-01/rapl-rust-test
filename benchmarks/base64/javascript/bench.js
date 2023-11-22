072573f3b570e45eab23a163df077dc20eeaf863
Jakobzs <31919330+Jakobzs@users.noreply.github.com>
2023-11-22 12:44:24 +0100

    fix bench py and js

--- benchmarks/base64/javascript/bench.js
+++ benchmarks/base64/javascript/bench.js
@@ -11,11 +11,42 @@ const lib = koffi.load(libPath);
 const start = lib.func('int start_rapl()');
 const stop = lib.func('void stop_rapl()');
 
+const STR_SIZE = 131072;
+const TRIES = 8192;
+
 for (let i = 0; i < runCount; i++) {
   start();
 
-  var str = "testy";
-  console.log(Buffer.from(str).toString('base64'));
+  const b = Buffer.from("a".repeat(STR_SIZE));
+  const str2 = b.toString('base64');
+  const str3 = Buffer.from(str2, 'base64');
+
+  var s_encoded = 0;
+  const start = new Date();
+  for (var i = 0; i < TRIES; i++) {
+    s_encoded += b.toString('base64').length;
+  }
+  const t_encoded = ((new Date()) - start) / 1000;
+
+  var s_decoded = 0;
+  const start1 = new Date();
+  for (var i = 0; i < TRIES; i++) {
+    s_decoded += Buffer.from(str2, 'base64').length;
+  }
+  const t_decoded = ((new Date()) - start1) / 1000;
+
+  console.log(util.format("encode %s... to %s...: %d, %d",
+    b.toString('utf8', 0, 4),
+    str2.substring(0, 4),
+    s_encoded, t_encoded));
+
+  console.log(util.format("decode %s... to %s...: %d, %d",
+    str2.substring(0, 4),
+    str3.toString('utf8', 0, 4),
+    s_decoded, t_decoded));
+
+  //var str = "testy";
+  //console.log(Buffer.from(str).toString('base64'));
 
   stop();
 }
--- benchmarks/base64/python/bench.py
+++ benchmarks/base64/python/bench.py
@@ -12,6 +12,31 @@ dll = cdll.LoadLibrary(lib_path)
 
 for i in range(test_count):
     dll.start_rapl()
-    str = "testy"
-    print(base64.b64encode(str))
+    STR_SIZE = 131072
+    TRIES = 8192
+
+    str1 = b"a" * STR_SIZE
+    str2 = base64.b64encode(str1)
+    str3 = base64.b64decode(str2)
+
+    t, s_encoded = time.time(), 0
+    for _ in range(0, TRIES):
+        s_encoded += len(base64.b64encode(str1))
+    t_encoded = time.time() - t
+
+    t, s_decoded = time.time(), 0
+    for _ in range(0, TRIES):
+        s_decoded += len(base64.b64decode(str2))
+    t_decoded = time.time() - t
+
+    print(
+        "encode {0}... to {1}...: {2}, {3}".format(
+            str1[:4], str2[:4], s_encoded, t_encoded
+        )
+    )
+    print(
+        "decode {0}... to {1}...: {2}, {3} ".format(
+            str2[:4], str3[:4], s_decoded, t_decoded
+        )
+    )
     dll.stop_rapl()
