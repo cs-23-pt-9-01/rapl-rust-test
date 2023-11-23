0e5ab5416261192a6c153fe70ccde66004a9720a
Jakobzs <31919330+Jakobzs@users.noreply.github.com>
2023-11-23 13:14:37 +0100

    c# bench fix

--- benchmarks/base64/csharp/Program.cs
+++ benchmarks/base64/csharp/Program.cs
@@ -25,45 +25,13 @@ const int TRIES = 8192;
 
 var str1 = Encoding.UTF8.GetBytes(new String('a', STR_SIZE));
 var str2 = Convert.ToBase64String(str1);
-var str3 = Convert.FromBase64String(str2);
 
 for (int i = 0; i < count; i++)
 {
- 
-    
-
-
-    var runtime = Type.GetType("Mono.Runtime") != null ? "Mono" : ".NET Core";
-
     start_rapl();
 
-    var sw = Stopwatch.StartNew();
-    var s_encoded = 0;
-    for (var i = 0; i < TRIES; i++)
-    {
-        s_encoded += Convert.ToBase64String(str1).Length;
-    }
-    sw.Stop();
-    var t_encoded = sw.Elapsed.TotalSeconds;
-
-    var s_decoded = 0;
-    sw.Restart();
-    for (var i = 0; i < TRIES; i++)
-    {
-        s_decoded += Convert.FromBase64String(str2).Length;
-    }
-    sw.Stop();
-    var t_decoded = sw.Elapsed.TotalSeconds;
+    Convert.ToBase64String(str1);
+    Convert.FromBase64String(str2);
 
     stop_rapl();
-
-    Console.WriteLine("encode {0}... {1}...: {2}, {3}",
-                        Encoding.UTF8.GetString(str1, 0, 4),
-                        str2.Substring(0, 4),
-                        s_encoded, t_encoded);
-    Console.WriteLine("decode {0}... {1}...: {2}, {3}",
-                        str2.Substring(0, 4),
-                        Encoding.UTF8.GetString(str3, 0, 4),
-                        s_decoded, t_decoded);
-    Console.WriteLine("overall time: {0}s", t_encoded + t_decoded);
 }
